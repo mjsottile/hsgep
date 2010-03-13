b3acedbfe95522bb3d8f790d8be1fa5572c93da1
Matthew Sottile <mjsottile@me.com>
2010-03-13 01:09:34 -0800

    docs

--- GEP/Rmonad.hs
+++ GEP/Rmonad.hs
@@ -1,12 +1,14 @@
 -- |
---  monad based on state for passing random
---  number state around for GEP
+--  Monad based on state for passing random number state around for GEP.
+--  The choice of Mersenne.Pure64 was for performance, and the pure version
+--  will play nicely with threading.
 -- 
 --  Author: mjsottile\@computer.org
 -- 
 
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GEP.Rmonad (
+    GEPMonad,
     nextF,
     nextR,
     nextRDifferent,
@@ -14,8 +16,7 @@ module GEP.Rmonad (
     nextRListUnique,
     nextRListPairs,
     generatePairs,
-    runRmonad,
-    GEPMonad
+    runRmonad
 ) where
 
 import System.Random.Mersenne.Pure64
@@ -25,28 +26,35 @@ import Debug.Trace
 newtype Rmonad s a = S (State s a)
     deriving (Monad)
 
+-- | The GEPMonad is just a specific instance of the State monad where the
+--   state is just the PureMT PRNG state.
 type GEPMonad a = Rmonad PureMT a
 
-nextF :: Double -> Rmonad PureMT Double
+-- | Generate a random number as a Double between 0.0 and the given upper
+--   bound.
+nextF :: Double -- ^ Upper bound.
+      -> Rmonad PureMT Double
 nextF up = S $ do st <- get
                   let (x,st') = randomDouble st
                   put st'
                   return (x*up)
 
-nextRList :: Int 
-          -> Int 
-          -> Rmonad PureMT [Int]
-nextRList 0 _  = do return []
-nextRList n up = do val <- nextR up
-                    vals <- nextRList (n-1) up
-                    return (val:vals)
-
-nextR :: Int 
+-- | Generate a random integer between 1 and the upper bound (inclusive).
+nextR :: Int -- ^ Upper bound.
       -> Rmonad PureMT Int
 nextR up = S $ do st <- get
                   let (x,st') = randomInt st
                   put st'
                   return (1 + ((abs x) `mod` up))
+
+-- | Generate a list of random integers.
+nextRList :: Int -- ^ Number of integers to generate
+          -> Int -- ^ Upper bound for each integer.
+          -> Rmonad PureMT [Int]
+nextRList 0 _  = do return []
+nextRList n up = do val <- nextR up
+                    vals <- nextRList (n-1) up
+                    return (val:vals)
 
 removeNth :: [a] -> Int -> [a]
 removeNth [] _ = []
@@ -64,12 +72,15 @@ pairify [] = []
 pairify (_:[]) = []
 pairify (x:y:xs) = ((x,y):(pairify xs))
 
+-- | Document me!
 generatePairs :: Int -> Rmonad PureMT [(Int,Int)]
 generatePairs 0 = do return []
 generatePairs 1 = do return []
 generatePairs n = do vals <- shuffle $! [1..n]
                      return $ (pairify vals)
 
+-- | Generate a list of n random integers such that each entry occurs at most
+--   once.  Each number in the list must be unique.
 nextRListUnique :: Int -> [Int] -> Int -> Rmonad PureMT [Int]
 nextRListUnique 0 l _  = do return l
 nextRListUnique n l up = do val <- nextR up
@@ -87,12 +98,17 @@ nextRListPairs n up = do val1 <- nextR up
                          rest <- nextRListPairs (n-1) up
                          return $ ((val1,val2):rest)
 
-nextRDifferent :: Int -> Int -> Rmonad PureMT Int
+-- | Generate a random integer in the specified range that is NOT equal to
+--   the integer provided.
+nextRDifferent :: Int -- ^ Upper bound.
+               -> Int -- ^ Integer to avoid.
+               -> Rmonad PureMT Int
 nextRDifferent up x = do x' <- nextR up
                          if x' == x
                             then do x'' <- nextRDifferent up x
                                     return x''
                             else return x'
 
+-- | Run function for the Rmonad.
 runRmonad :: Rmonad PureMT a -> PureMT -> (a, PureMT)
 runRmonad (S m) s = runState m s
