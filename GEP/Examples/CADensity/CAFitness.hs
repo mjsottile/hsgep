1cc161aa41ae3aced69e4636e96c7c9801ee50cc
Matthew Sottile <matt@matt.galois.com>
2010-04-09 14:56:32 -0700

    fixes

--- GEP/Examples/CADensity/CAFitness.hs
+++ GEP/Examples/CADensity/CAFitness.hs
@@ -1,6 +1,7 @@
 module GEP.Examples.CADensity.CAFitness(
   CA1D(..),
-  nsteps
+  nsteps,
+  agreement
 ) where
 
 import qualified Data.Vector as U
@@ -15,7 +16,7 @@ idx i w = i `mod` w
 type CA7Rule = (Bool,Bool,Bool,Bool,Bool,Bool,Bool) -> Bool
 
 onestep :: CA7Rule -> CA1D -> CA1D
-onestep rule (CA1D state) = U.imap (\i -> site i) state
+onestep rule (CA1D state) = CA1D $ U.imap (\i _ -> site i) state
   where
     w = U.length state
     site i = rule (l3,l2,l1,c,r1,r2,r3)
@@ -32,10 +33,9 @@ nsteps rule state n = last $ take n (iterate (onestep rule) state)
 
 agreement :: CA1D -> Maybe Bool
 agreement (CA1D state) =
-  case count of
-    0 -> Just False
-    w -> Just True
-    otherwise -> Nothing
+  if count==0 then Just False
+              else if count==w then Just True
+                               else Nothing
   where 
     w = U.length state
     count = U.foldl (\c s -> if s then c+1 else c) 0 state
