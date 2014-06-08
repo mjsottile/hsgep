1f887ddf07f2a57bb49537154fd553c0aa1051ab
andreatrygit <andreatrygit@users.noreply.github.com>
2014-06-08 14:45:45 +0200

    Update ConfigurationReader.hs
    Wrapped readParameters result in a Either with a String, which can report an error message.
    Added some functions to check the Config file is sound.
    Provided a curriable function to read strings into custom types.

--- GEP/Util/ConfigurationReader.hs
+++ GEP/Util/ConfigurationReader.hs
@@ -1,7 +1,8 @@
 -- |
 -- Code to read configuration files.
 --
--- Author: mjsottile\@computer.org
+-- Authors: mjsottile\@computer.org
+--	  : andreawscotti@gmail.com	
 --
 
 module GEP.Util.ConfigurationReader (
@@ -13,106 +14,124 @@ import GEP.Types
 import System.IO
 import Data.Maybe
 
---
--- given a list of pairs mapping keys to values, lookup the various
--- parameters and populate the rates, genome, and simparams structures
---
+-- function visible to the outside world.  passes in a string representing
+-- the filepath to the configuration file, and passes back the rates,
+-- genome, and simparams structures.  Expected to be called from within the
+-- IO monad.
+
+--The result is wrapped in a Either together with a String. If result is Right x, then x is a human readable error message.
+
+
+readParameters :: String -> IO (Either (Rates,Genome,SimParams) String)
+readParameters path = do
+	s <- readFile path
+	case (checkConfig s) of
+		Right er -> return (Right er)			
+		Left _ -> return $ Left $ extractParameters $ readStringIntoDict s
+
+-- creates a dict out of a String.
+readStringIntoDict::String->[(String->String)]
+readStringIntoDict = makeDict.clean
+
 extractParameters :: [(String,String)] -> (Rates,Genome,SimParams)
 extractParameters config = (r,g,s)
+
     where
+
       s = SimParams { 
-	    popSize          = fromJust (lookupInt "populationSize" config),
-	    selectionRange   = fromJust (lookupDouble "selectionRange" config),
-	    maxFitness       = fromJust (lookupDouble "maxFitness" config),
-	    numGenerations   = fromJust (lookupInt "numGenerations" config),
-	    maxISLen         = fromJust (lookupInt "maxISLen" config),
-	    maxRISLen        = fromJust (lookupInt "maxRISLen" config),
+	    popSize          = fromJust (lookupInt    "populationSize"   config),
+	    selectionRange   = fromJust (lookupDouble "selectionRange"   config),
+	    maxFitness       = fromJust (lookupDouble "maxFitness"       config),
+	    numGenerations   = fromJust (lookupInt    "numGenerations"   config),
+	    maxISLen         = fromJust (lookupInt    "maxISLen"         config),
+	    maxRISLen        = fromJust (lookupInt    "maxRISLen"        config),
 	    rouletteExponent = fromJust (lookupDouble "rouletteExponent" config) 
 	  }
-      r = Rates { pMutate = fromJust (lookupDouble "rateMutate" config),
-	          p1R     = fromJust (lookupDouble "rate1R" config),
-	          p2R     = fromJust (lookupDouble "rate2R" config),
-	          pGR     = fromJust (lookupDouble "rateGR" config),
-	          pIS     = fromJust (lookupDouble "rateIS" config),
-	          pRIS    = fromJust (lookupDouble "rateRIS" config),
-	          pGT     = fromJust (lookupDouble "rateGT" config) 
-	        }
-      g = Genome { 
-	    terminals     = fromJust (lookupString "genomeTerminals" config),
-	    nonterminals  = fromJust (lookupString "genomeNonterminals" config),
-	    geneConnector = fromJust (lookupChar "genomeGeneConnector" config),
-	    maxArity      = fromJust (lookupInt "genomeMaxArity" config),
-	    numGenes      = fromJust (lookupInt "genomeNumGenes" config),
-	    headLength    = fromJust (lookupInt "genomeHeadLength" config)
+
+      r = Rates     {           
+	             pMutate = fromJust (lookupDouble "rateMutate" config),
+	             p1R     = fromJust (lookupDouble "rate1R"     config),
+	             p2R     = fromJust (lookupDouble "rate2R"     config),
+	             pGR     = fromJust (lookupDouble "rateGR"     config),
+	             pIS     = fromJust (lookupDouble "rateIS"     config),
+	             pRIS    = fromJust (lookupDouble "rateRIS"    config),
+	             pGT     = fromJust (lookupDouble "rateGT"     config) 
 	  }
 
---
--- function visible to the outside world.  passes in a string representing
--- the filename of the configuration, and passes back the rates,
--- genome, and simparams structures.  Expected to be called from within the
--- IO monad
---
-readParameters :: String -> IO (Rates,Genome,SimParams)
-readParameters filename = 
-	do config <- readConfiguration filename
-	   return $ extractParameters config
+      g = Genome    { 
+	       terminals     = fromJust (lookupString "genomeTerminals"     config),
+	       nonterminals  = fromJust (lookupString "genomeNonterminals"  config),
+	       geneConnector = fromJust (lookupChar   "genomeGeneConnector" config),
+	       maxArity      = fromJust (lookupInt    "genomeMaxArity"      config),
+	       numGenes      = fromJust (lookupInt    "genomeNumGenes"      config),
+	       headLength    = fromJust (lookupInt    "genomeHeadLength"    config)
+	  }
 
---
--- lookup helpers: float, int, char, and string versions
---
 
-lookupDouble :: String -> [(String,String)] -> Maybe Double
-lookupDouble k dict = 
-	case (lookup k dict) of
-		Nothing -> Nothing
-		Just s  -> Just (read s)
+-- checks Config File is sound. Reports first error on failures.
+checkConfig::String->Either Bool String
+checkConfig s = multicheck s [isNotEmpty,checkNewLines,eitherAnd.(map checkDelimiter).lines,eitherAnd.(map checkMinLineLength).lines]
 
-lookupInt :: String -> [(String,String)] -> Maybe Int
-lookupInt k dict =
-	case (lookup k dict) of
-		Nothing -> Nothing
-		Just s  -> Just (read s)
+-- makes a dict out of a long String
+makeDict::String->[(String,String)]
+makeDict s =  map mySplit $ lines s
 
-lookupString :: String -> [(String,String)] -> Maybe String
-lookupString k dict = lookup k dict
+-- splits a line based on '=' char
+mySplit::String->(String,String)
+mySplit s = (a,b) where
+	a = takeWhile (/='=') s
+	b = drop (length a +1) s
 
-lookupChar :: String -> [(String,String)] -> Maybe Char
-lookupChar k dict = 
-	case (lookup k dict) of
-		Nothing -> Nothing
-		Just s  -> Just (head s)
+-- checks a type against many predicates. The latter are a List of Either Bool String, so you get Left True or a Right String with an error message. It stops at first failed check.
+multicheck::a->[a->Either Bool String]->Either Bool String
+multicheck x [] = Left True
+multicheck x (f:fs) = case (f x) of
+		Left _ -> multicheck x fs
+		Right s -> Right s
 
---
--- given a string, remove whitespace
---
-removeWhitespace :: String -> String
-removeWhitespace s = filter (\x -> x /= ' ' && x /= '\t') s
+-- checks a list is not empty. 
+isNotEmpty::(Show a)=>[a]->Either Bool String
+isNotEmpty l = if null l then Right "List is empty on imput." else Left True
 
---
--- split a line formatted as "KEY=VALUE", removing whitespace
---
-splitLine :: String -> (String,String)
-splitLine l = (front,back)
-  where
-    cleaned = removeWhitespace l
-    front   = takeWhile (\i -> i /= '=') cleaned
-    back    = drop (1 + length front) cleaned
---
--- read a file handle and return all of the lines in the file
---
-fileToLines :: Handle -> IO [String]
-fileToLines h = do eof <- hIsEOF h
-                   (if eof
-                    then return []
-                    else do line <- hGetLine h
-                            remainder <- fileToLines h
-                            return $ (line:remainder))
+-- checks there are no empty lines interspersed into a bigger String.
+checkNewLines::String-> Either Bool String
+checkNewLines s = if (not $ elem "" $ lines s) then Left True else Right $ "Failed checkNewLines on input:\n\n" ++ s ++ "\n"
 
---
--- given a filename, open the file, read the lines, and then split them
--- into key/value pairs assuming a "KEY=VALUE" format per line
---
-readConfiguration :: String -> IO [(String,String)]
-readConfiguration filename = do handle <- openFile filename ReadMode
-                                fileLines <- fileToLines handle
-                                return $ map splitLine fileLines
+-- checks there is exactly one '=' char in a String.
+checkDelimiter::String->Either Bool String
+checkDelimiter s = let c = count '=' s in 
+		if c==1 then Left True else Right $ "Delimiter count is not 1, it is " ++ (show c) ++ " in input:\n\n" ++ s ++ "\n"
+
+-- checks a String is long enough to carry a key value pair.
+checkMinLineLength::String->Either Bool String
+checkMinLineLength s = if (length s)>2 then Left True else Right "Line too short to represent a key value pair on input:\n\n" ++ s ++ "\n"
+
+-- this is like the 'and' function on lists. It just takes a list of Either Bool String; it returns a Right String, which is extracted from the very first Right String it encounters, otherwise it returns Left True.
+eitherAnd::[Either Bool a]->Either Bool a
+eitherAnd [] = Left True
+eitherAnd (x:xs) = case x of
+	Left _ -> eitherAnd xs
+	Right a -> Right a
+
+-- remove every occurrence of ANY Char in the first String, from the second one.	
+remove::String->String->String
+remove _ "" = ""
+remove xs (y:ys) = if elem y xs then remove xs ys else y : remove xs ys
+
+-- removes whitespaces and tabs, curried version of remove.
+clean = remove (" " ++ "\t")
+
+-- count things in lists.
+count::(Eq a)=>a->[a]->Int
+count _ [] = 0
+count x (y:ys) = if x==y then 1 + (count x ys) else count x ys
+
+-- generic lookup fuction, implemented with monadic code.
+lookUpThing:: (Read a) => (String-> Maybe a)->String -> [(String,String)] -> Maybe a
+lookUpThing f k dict = (lookup k dict) >>= f
+
+-- curried versions of LookUpThing, to serve as utils.
+lookupInt = lookUpThing (\x -> Just(read x :: Int))
+lookupDouble = lookUpThing (\x -> Just(read x :: Double))
+lookupChar = lookUpThing (\x->Just(head x))
+lookupString = lookUpThing (\x->Just(id x))
