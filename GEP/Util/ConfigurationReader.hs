388d0f6e9ced0de3167b40f8d58864541de7a61a
Matthew Sottile <matt@galois.com>
2014-07-08 09:48:51 -0700

    big clean up after pull request accept.  major issue was unconventional use of Either -- convention is that the "right" answer is Right, and the error is Left.  this has been repaired to adhere to common convention. also, performed some aesthetic cleanup and squashed compiler warnings.

--- GEP/Util/ConfigurationReader.hs
+++ GEP/Util/ConfigurationReader.hs
@@ -11,7 +11,7 @@ module GEP.Util.ConfigurationReader (
 
 import GEP.Params
 import GEP.Types
-import System.IO
+--import System.IO
 import Data.Maybe
 
 -- function visible to the outside world.  passes in a string representing
@@ -19,18 +19,18 @@ import Data.Maybe
 -- genome, and simparams structures.  Expected to be called from within the
 -- IO monad.
 
---The result is wrapped in a Either together with a String. If result is Right x, then x is a human readable error message.
+-- The result is wrapped in a Either together with a String. If result
+-- is Left x, then x is a human readable error message.
 
-
-readParameters :: String -> IO (Either (Rates,Genome,SimParams) String)
+readParameters :: String -> IO (Either String (Rates,Genome,SimParams) )
 readParameters path = do
 	s <- readFile path
 	case (checkConfig s) of
-		Right er -> return (Right er)			
-		Left _ -> return $ Left $ extractParameters $ readStringIntoDict s
+		Left er -> return (Left er)			
+		Right _ -> return $ Right $ extractParameters $ readStringIntoDict s
 
 -- creates a dict out of a String.
-readStringIntoDict::String->[(String->String)]
+readStringIntoDict :: String -> [(String,String)]
 readStringIntoDict = makeDict.clean
 
 extractParameters :: [(String,String)] -> (Rates,Genome,SimParams)
@@ -69,49 +69,58 @@ extractParameters config = (r,g,s)
 
 
 -- checks Config File is sound. Reports first error on failures.
-checkConfig::String->Either Bool String
-checkConfig s = multicheck s [isNotEmpty,checkNewLines,eitherAnd.(map checkDelimiter).lines,eitherAnd.(map checkMinLineLength).lines]
+checkConfig :: String -> Either String Bool
+checkConfig s = 
+  multicheck s [isNotEmpty, checkNewLines, 
+                eitherAnd.(map checkDelimiter).lines, 
+                eitherAnd.(map checkMinLineLength).lines]
 
 -- makes a dict out of a long String
-makeDict::String->[(String,String)]
+makeDict :: String -> [(String,String)]
 makeDict s =  map mySplit $ lines s
 
 -- splits a line based on '=' char
-mySplit::String->(String,String)
+mySplit :: String -> (String,String)
 mySplit s = (a,b) where
 	a = takeWhile (/='=') s
 	b = drop (length a +1) s
 
 -- checks a type against many predicates. The latter are a List of Either Bool String, so you get Left True or a Right String with an error message. It stops at first failed check.
-multicheck::a->[a->Either Bool String]->Either Bool String
-multicheck x [] = Left True
+multicheck :: a -> [a -> Either String Bool] -> Either String Bool
+multicheck _ []     = Right True
 multicheck x (f:fs) = case (f x) of
-		Left _ -> multicheck x fs
-		Right s -> Right s
+		Right _ -> multicheck x fs
+		Left s -> Left s
 
 -- checks a list is not empty. 
-isNotEmpty::(Show a)=>[a]->Either Bool String
-isNotEmpty l = if null l then Right "List is empty on imput." else Left True
+isNotEmpty :: (Show a) => [a] -> Either String Bool
+isNotEmpty l = if null l then Left "List is empty on imput." 
+                         else Right True
 
 -- checks there are no empty lines interspersed into a bigger String.
-checkNewLines::String-> Either Bool String
-checkNewLines s = if (not $ elem "" $ lines s) then Left True else Right $ "Failed checkNewLines on input:\n\n" ++ s ++ "\n"
+checkNewLines :: String -> Either String Bool
+checkNewLines s = if (not $ elem "" $ lines s) then Right True 
+                                               else Left $ "Failed checkNewLines on input:\n\n" ++ s ++ "\n"
 
 -- checks there is exactly one '=' char in a String.
-checkDelimiter::String->Either Bool String
-checkDelimiter s = let c = count '=' s in 
-		if c==1 then Left True else Right $ "Delimiter count is not 1, it is " ++ (show c) ++ " in input:\n\n" ++ s ++ "\n"
+checkDelimiter :: String -> Either String Bool
+checkDelimiter s = 
+  let c = count '=' s in 
+  if c==1 then Right True 
+          else Left $ "Delimiter count is not 1, it is " ++ (show c) ++ " in input:\n\n" ++ s ++ "\n"
 
 -- checks a String is long enough to carry a key value pair.
-checkMinLineLength::String->Either Bool String
-checkMinLineLength s = if (length s)>2 then Left True else Right "Line too short to represent a key value pair on input:\n\n" ++ s ++ "\n"
+checkMinLineLength :: String -> Either String Bool
+checkMinLineLength s = 
+  if (length s) > 2 then Right True 
+                    else Left $ "Line too short to represent a key value pair on input:\n\n" ++ s ++ "\n"
 
 -- this is like the 'and' function on lists. It just takes a list of Either Bool String; it returns a Right String, which is extracted from the very first Right String it encounters, otherwise it returns Left True.
-eitherAnd::[Either Bool a]->Either Bool a
-eitherAnd [] = Left True
+eitherAnd::[Either a Bool]->Either a Bool
+eitherAnd [] = Right True
 eitherAnd (x:xs) = case x of
-	Left _ -> eitherAnd xs
-	Right a -> Right a
+	Right _ -> eitherAnd xs
+	Left a -> Left a
 
 -- remove every occurrence of ANY Char in the first String, from the second one.	
 remove::String->String->String
@@ -119,10 +128,11 @@ remove _ "" = ""
 remove xs (y:ys) = if elem y xs then remove xs ys else y : remove xs ys
 
 -- removes whitespaces and tabs, curried version of remove.
+clean :: String -> String
 clean = remove (" " ++ "\t")
 
 -- count things in lists.
-count::(Eq a)=>a->[a]->Int
+count :: (Eq a) => a -> [a] -> Int
 count _ [] = 0
 count x (y:ys) = if x==y then 1 + (count x ys) else count x ys
 
@@ -131,7 +141,14 @@ lookUpThing:: (Read a) => (String-> Maybe a)->String -> [(String,String)] -> May
 lookUpThing f k dict = (lookup k dict) >>= f
 
 -- curried versions of LookUpThing, to serve as utils.
+lookupInt :: String -> [(String, String)] -> Maybe Int
 lookupInt = lookUpThing (\x -> Just(read x :: Int))
+
+lookupDouble :: String -> [(String, String)] -> Maybe Double
 lookupDouble = lookUpThing (\x -> Just(read x :: Double))
+
+lookupChar :: String -> [(String, String)] -> Maybe Char
 lookupChar = lookUpThing (\x->Just(head x))
+
+lookupString ::  String -> [(String, String)] -> Maybe String
 lookupString = lookUpThing (\x->Just(id x))
